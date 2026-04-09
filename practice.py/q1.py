4c194db2079ea653db3faddc23f42df9e2b8f979
Rajesh_Rayal <rayalrajesh6@gmail.com>
2026-04-09 17:43:11 +0530

    Add files via upload

---
+++ practice.py/q1.py
@@ -0,0 +1,20 @@
+t=int(input())
+for i in range(t):
+    n,k=map(int,input().split())
+    arr=list(map(int,input().split()))
+    x=int(input()) - 1
+    right=0
+    if arr[n-1] != arr[x] :
+        right = 1
+    for i in range(x+1,n):
+        if arr[i] != arr[i-1] :
+            right +=1
+    
+    left=0
+    if arr[0] != arr[x] :
+        left =1
+    for i in range(x-1,-1,-1):
+        if arr[i] != arr[i+1] :
+            left +=1
+    
+    print("ans ",max(right,left))
